c213cf1eeeb22479ec682afa9457cadb66e0c4ca
Marek Jakubowski <15730464+vertiavo@users.noreply.github.com>
2025-09-29 10:15:58 +0000

    Fix failing assertion

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -6,7 +6,8 @@ import os
 import pytest
 
 # Project Modules
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '../src')))
 from calculations import area_of_circle, get_nth_fibonacci   # noqa: E402
 
 
@@ -67,4 +68,4 @@ def test_get_nth_fibonacci_ten():
     result = get_nth_fibonacci(n)
 
     # Assert
-    assert result == 89
+    assert result == 55
